2a02e4800ce602beb457511384a54a19ddfa0b8a
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-10-23 10:14:25 -0400

    Initial Commit

---
+++ IOSApp3 Watch App/IOSApp3App.swift
@@ -0,0 +1,17 @@
+//
+//  IOSApp3App.swift
+//  IOSApp3 Watch App
+//
+//  Created by Jose Flores on 2025-10-23.
+//
+
+import SwiftUI
+
+@main
+struct IOSApp3_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

069ec69a4f61f33521558c6644f5145337e64c4f
JoseFlores <joseolmedo.florespaniagua@triosstudent.com>
2025-10-23 10:28:15 -0400

    Setup basic app entry point with MindfulGraceApp

---
+++ IOSApp3 Watch App/Models/MotivationalQuote.swift
@@ -0,0 +1,8 @@
+//
+//  MotivationalQuote.swift
+//  IOSApp3 Watch App
+//
+//  Created by Jose Flores on 2025-10-23.
+//
+
+import Foundation
---
+++ IOSApp3 Watch App/Views/BreathingSessionView.swift
@@ -0,0 +1,18 @@
+//
+//  BreathingSessionView.swift
+//  IOSApp3 Watch App
+//
+//  Created by Jose Flores on 2025-10-23.
+//
+
+import SwiftUI
+
+struct BreathingSessionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    BreathingSessionView()
+}
